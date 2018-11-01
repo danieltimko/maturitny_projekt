434b3d10b71a2f0f012e9259ae4fd7abf83638e2
Daniel <daniel.timko4@gmail.com>
2018-11-02 00:04:26 +0100

    floyd-warshall

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 
+#define inf 0x3f3f3f3f
+
 using namespace std;
 
 struct Schedule{
@@ -10,19 +12,22 @@ struct Schedule{
 };
 
 fstream file;
-vector<vector<int>> G;
+vector<vector<int>> dist;
 vector<Schedule> schedules;
+
 
 void read_graph(){
     file.open("../graph.txt");
     int nodes, edges;
     file >> nodes >> edges;
-    G.resize(nodes+1);
+    dist.resize(nodes+1);
+    for (int i = 0; i < nodes+1; ++i)
+        dist[i].resize(nodes+1, inf);
     for (int i = 0; i < edges; ++i) {
-        int from, to;
-        file >> from >> to;
-        G[from].push_back(to);
-        G[to].push_back(from);
+        int from, to, cost;
+        file >> from >> to >> cost;
+        dist[from][to] = cost;
+        dist[to][from] = cost;
     }
     file.close();
 }
@@ -38,9 +43,9 @@ void read_schedules(){
             int periods; //pocet hodin v dany den
             file >> periods;
             for (int j = 0; j < periods; ++j) {
-                int period; //ucebna v ktorej je hodina
-                file >> period;
-                schedules[i].schedule[day].push_back(period);
+                int classroom; //ucebna v ktorej je hodina
+                file >> classroom;
+                schedules[i].schedule[day].push_back(classroom);
             }
         }
     }
@@ -48,12 +53,13 @@ void read_schedules(){
 }
 
 void print_graph(){
-    for(int i = 1; i < G.size(); ++i) {
-        cout << "susedia vrcholu " << i << ": ";
-        for (int j : G[i])
-            cout << j << " ";
-        cout << endl;
-    }
+    for (int i = 1; i < dist.size(); ++i)
+        for (int j = 1; j < dist.size(); ++j) {
+            if (dist[i][j] == inf)
+                cout << '-';
+            else cout << dist[i][j];
+            cout << " \n"[j == dist.size()-1];
+        }
     cout << endl;
 }
 
@@ -68,15 +74,27 @@ void print_schedules(){
     cout << endl;
 }
 
+void make_floyd(){
+    for (int k = 1; k < dist.size(); ++k)
+        for (int i = 1; i < dist.size(); ++i)
+            for (int j = 1; j < dist.size(); ++j)
+                if (dist[i][j] > dist[i][k] + dist[k][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+}
+
 int main() {
     read_graph();
     read_schedules();
-    //print_graph();
-    //print_schedules();
+    print_graph();
+    print_schedules();
+    make_floyd();
+    print_graph();
 
-    //TODO ceny ciest, floyd-warshall
-    //TODO ?riesenie pre jednu triedu?
-    //TODO prechadzanie vsetkych permutacii
-    //TODO penalty pre kazdu permutaciu
-    //TODO preskakovanie nevyhodnych permutacii
+    /*
+    TODO ceny ciest, floyd-warshall
+    TODO ?riesenie pre jednu triedu?
+    TODO prechadzanie vsetkych permutacii
+    TODO penalty pre kazdu permutaciu
+    TODO preskakovanie nevyhodnych permutacii
+    */
 }
